b362b48ec2c30cf6b4124e52a44e4825cec81acd
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-11-26 13:07:29 +0100

    Change in include_sample not reinitiate LDS parameters.

--- hdpgpc/tests/test_online.py
+++ hdpgpc/tests/test_online.py
@@ -47,9 +47,9 @@ std, std_dif = compute_estimators_LDS(data, n_f)
 #Outputscale is the max amplitude of the records (if data is standardized it can be set to 1.0)
 M = 2
 sigma = [std * 1.0] * M
-bound_sigma_ = (std * 0.01, std * 0.2)
+bound_sigma_ = (std * 0.1, std * 0.2)
 gamma = [std_dif * 1.0] * M
-bound_gamma = (std_dif * 0.01, std_dif * 0.2)
+bound_gamma = (std_dif * 0.1, std_dif * 0.2)
 outputscale_ = 300.0
 ini_lengthscale = 3.0
 bound_lengthscale = (1.0, 20.0)
