b2289f12b9613623dc447db004edc0fd2883973e
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-11 18:04:13 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -267,14 +267,14 @@ class GPI_model():
             lat_f_ = self.f_star_sm[i]
         #cov_f = self.cov_f_sm[i + 1]
         lat_f = self.f_star_sm[i + 1]
-        Gamma = self.Gamma[-1]
-        A = self.A[-1]
+        Gamma = self.Gamma[i + 1]
+        A = self.A[i + 1]
         #t = Gamma.shape[0]
         exp_t_t_ = cov_f_ + torch.matmul(lat_f_, lat_f_.T)
         #xp_t_t = cov_f + torch.matmul(lat_f, lat_f.T)
         #A = self.A[i + 1]
-        #Gamma_inv = torch.linalg.solve(Gamma, self.cond_to_cuda(torch.eye(Gamma.shape[0])))
-        Gamma_inv = self.gamma_inv
+        Gamma_inv = torch.linalg.solve(Gamma, self.cond_to_cuda(torch.eye(Gamma.shape[0])))
+        #Gamma_inv = self.gamma_inv
         err = -1 / 2 * torch.linalg.multi_dot([lat_f.T, Gamma_inv, lat_f]) \
               + torch.linalg.multi_dot([lat_f.T, Gamma_inv, A, lat_f_]) \
               - 1 / 2 * torch.trace(torch.linalg.multi_dot([A.T, Gamma_inv, A, exp_t_t_])) \
@@ -455,7 +455,6 @@ class GPI_model():
     def compute_q_lat_all(self, x_trains, t=None):
         """Method to compute the latent squared error accumulated.
         """
-        self.backwards()
         sq_err = torch.zeros(1, device=x_trains[0].device)
         if self.N == 0:
             return sq_err
