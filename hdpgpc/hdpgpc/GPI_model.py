1e613a80e4a6f3725c7edf3e24670107dc570744
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-11 10:26:20 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -264,8 +264,8 @@ class GPI_model():
             lat_f_ = self.f_star_sm[i]
         cov_f = self.cov_f_sm[i + 1]
         lat_f = self.f_star_sm[i + 1]
-        Gamma = self.Gamma[-1]
-        A = self.A[-1]
+        Gamma = self.Gamma[i + 1]
+        A = self.A[i + 1]
         #t = Gamma.shape[0]
         exp_t_t_ = cov_f_ + torch.matmul(lat_f_, lat_f_.T)
         exp_t_t = cov_f + torch.matmul(lat_f, lat_f.T)
