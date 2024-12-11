322db5eaffa759a76bf40ab55188490bf6525b57
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-11 11:37:01 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1220,7 +1220,7 @@ class GPI_HDP():
         """ Method to compute ELBO terms.
         """
         q_bas = torch.sum(q[torch.where(resp.int() > 0.99)])
-        elbo_latent = torch.sum(q_lat)
+        elbo_latent = torch.sum(q_lat) / q.shape[0]
         if prev:
             if torch.sum(resp, dim=0)[-1] < 1.0 and self.M > 1:
                 elbo_bas = self.elbo_Linears(resp, respPair, prev=prev)
@@ -1278,9 +1278,9 @@ class GPI_HDP():
             model_type, recursive_warp, warp_updating, inducing_points, estimation_limit, free_deg_MNIV = self.get_default_options()
         # ini_Sigma = var_y_y * 0.15
         # ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.15
-        ini_Sigma = var_y_y * 0.5
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.0]), var_y_y * 2.0])) * 1.5
-        #ini_Gamma = var_y_y_ * 1.0
+        ini_Sigma = var_y_y * 1.0
+        #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.0]), var_y_y * 2.0])) * 1.5
+        ini_Gamma = var_y_y_ * 1.0
         #ini_Sigma = 50.0
         #ini_Gamma = 60.0
         #ini_Sigma = self.cond_to_torch(np.min([ini_Sigma, 45.0]))
