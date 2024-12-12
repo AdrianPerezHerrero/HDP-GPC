cd23fa0154db77710de53a66f70b7323efb09689
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-12 17:18:57 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -1275,8 +1275,8 @@ class matrix_normal_inv_wishart():
         #Scale with dimension:
         #scale_lik = scale_lik / self.scale.shape[0]
         #return scale_lik# / d
-        #return (mean_lik + scale_lik) / d**2
-        return mean_lik + scale_lik
+        return (mean_lik + scale_lik) / d
+        #return mean_lik + scale_lik
 
     def get_mean(self):
         return self.m_mean
