eedadb0c735c5641e547835211a120e435dbb406
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-13 17:28:09 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1031,8 +1031,10 @@ class GPI_HDP():
                                                         gpmodels_temp, M, snr=snr_aux, prev=True)
             update_snr = True
             print("Sum resp_temp: " + str(torch.sum(resp_temp, dim=0)))
-            print("Q_bas: " + str(q_bas) + ", Elbo_bas: " + str(elbo_bas))
-            print("Q_bas_post: " + str(q_bas_post) + ", Elbo_post: " + str(elbo_post))
+            print("Q_bas: " + str(q_bas) + ", Q_lat: " + str(
+                torch.sum(self.weight_mean(q_lat_, snr_))) + ", Elbo_bas: " + str(elbo_bas))
+            print("Q_bas_post: " + str(q_bas_post) + ", Q_lat: " + str(
+                torch.sum(self.weight_mean(q_lat, snr_aux))) + ", Elbo_post: " + str(elbo_post))
             if q_bas < q_bas_post:
                 if not q_bas + elbo_bas < q_bas_post + elbo_post:
                     print("Possibly better but no.")
@@ -1198,8 +1200,8 @@ class GPI_HDP():
                     update_snr = True
 
                     print("Sum resp_temp: "+str(torch.sum(resp_temp,dim=0)))
-                    print("Q_bas: " + str(q_bas) + ", Elbo_bas: " + str(elbo_bas))
-                    print("Q_bas_post: " + str(q_bas_post) + ", Elbo_post: " + str(elbo_post))
+                    print("Q_bas: " + str(q_bas) + ", Q_lat: " + str(torch.sum(self.weight_mean(q_lat_, snr_))) + ", Elbo_bas: " + str(elbo_bas))
+                    print("Q_bas_post: " + str(q_bas_post) + ", Q_lat: " + str(torch.sum(self.weight_mean(q_lat, snr_aux)))+ ", Elbo_post: " + str(elbo_post))
                     if q_bas < q_bas_post:
                         if not q_bas + elbo_bas < q_bas_post + elbo_post:
                             print("Possibly better but no.")
@@ -1286,7 +1288,7 @@ class GPI_HDP():
         # ini_Sigma = var_y_y * 0.15
         # ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.15
         ini_Sigma = var_y_y * 1.0
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.0]), var_y_y * 2.0])) * 1.2
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.0]), var_y_y * 2.0])) * 1.3
         #ini_Gamma = var_y_y_ * 1.0
         #ini_Sigma = 50.0
         #ini_Gamma = 60.0
