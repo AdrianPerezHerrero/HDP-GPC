c93cd35bb4cb93920f36f1830280df402c516bc2
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-13 13:23:03 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1286,7 +1286,7 @@ class GPI_HDP():
         # ini_Sigma = var_y_y * 0.15
         # ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.15
         ini_Sigma = var_y_y * 1.0
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.2]), var_y_y * 2.0])) * 1.0
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_, var_y_y * 1.0]), var_y_y * 2.0])) * 1.2
         #ini_Gamma = var_y_y_ * 1.0
         #ini_Sigma = 50.0
         #ini_Gamma = 60.0
