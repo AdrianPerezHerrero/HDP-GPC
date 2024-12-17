e0e71156771ac1273242e4dd434eae5698b513d5
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-17 19:04:15 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -268,8 +268,8 @@ class GPI_model():
             cov_f_ = self.cov_f_sm[i]
             lat_f_ = self.f_star_sm[i]
             if i+1 < len(self.Gamma):
-                Gamma_inv = torch.linalg.solve(self.Gamma[i+1], self.cond_to_cuda(torch.eye(self.Gamma[i].shape[0])))
-                A = self.A[i+1]
+                Gamma_inv = torch.linalg.solve(self.Gamma[-1], self.cond_to_cuda(torch.eye(self.Gamma[i].shape[0])))
+                A = self.A[-1]
             else:
                 Gamma_inv = self.gamma_inv
                 A = self.A[-1]
@@ -563,7 +563,7 @@ class GPI_model():
                     Sigma = Sigma + self.Gamma[-1]
                 mean = torch.matmul(C, self.f_star_sm[t])
             else:
-                A, Gamma, C, Sigma = self.get_params(t)
+                A, Gamma, C, Sigma = self.get_params(-1)
                 if proj:
                     Sigma = Sigma + Gamma
                 mean = torch.matmul(C, self.f_star_sm[t])
