3b1961e4debdaec3f3c04a0ddfb596bdcd86cd1f
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-18 10:52:10 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -364,7 +364,7 @@ class GPI_model():
                                              y_trains[index], resp[index])#, snr=snr_)
                 #self.backwards_pair(resp[index])  # , snr=snr_)
                 self.bayesian_new_params(resp[index])
-        #self.backwards()
+        self.backwards()
         #self.bayesian_new_params(1.0, full_data=True)
         #self.backwards()
         q_ = self.compute_sq_err_all(x_trains, y_trains)
