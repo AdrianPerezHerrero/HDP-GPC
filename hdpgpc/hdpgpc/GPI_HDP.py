e4ea72eece33ebf3c7d34fc09b22233e656c349a
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-19 12:56:26 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -242,14 +242,14 @@ class GPI_HDP():
         # self.transAlpha = 1400.0
         # self.startAlpha = 1400.0
         # self.kappa = 0.0
-        self.gamma = 100.0
-        self.transAlpha = 200.0
-        self.startAlpha = 200.0
-        self.kappa = 0.0
-        # self.gamma = 1.0
-        # self.transAlpha = 2.0
-        # self.startAlpha = 2.0
+        # self.gamma = 100.0
+        # self.transAlpha = 200.0
+        # self.startAlpha = 200.0
         # self.kappa = 0.0
+        self.gamma = 1.0
+        self.transAlpha = 2.0
+        self.startAlpha = 2.0
+        self.kappa = 0.0
 
         # Model associated with each state
         # Default hyperparameters of GP defining the model.
