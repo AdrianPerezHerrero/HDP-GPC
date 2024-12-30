ed960ffe8a463d7f8b310c64be21bd52314826ab
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-30 01:45:13 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1302,8 +1302,8 @@ class GPI_HDP():
         ini_Gamma = self.cond_to_torch(np.min(([ini_Gamma, 300.0])))
         #ini_Gamma = self.cond_to_torch(np.max([var_y_y_, var_y_y * 1.5]))
         #ini_Gamma = var_y_y_ * 1.5
-        bound_sigma = (ini_Sigma * 0.05, ini_Sigma * 1.0)
-        bound_gamma = (ini_Gamma * 0.05, ini_Gamma * 1.0)
+        bound_sigma = (ini_Sigma * 0.01, ini_Sigma * 1.0)
+        bound_gamma = (ini_Gamma * 0.01, ini_Gamma * 1.0)
         # bound_sigma = (0.1, 20.0)
         # bound_gamma = (0.1, 20.0)
         print("-----------Reestimated -----------", flush=True)
