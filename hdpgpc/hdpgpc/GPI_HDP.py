cdc78b0ebb1e0dd0fd97d631624a1ae25f03f110
adrianperezherrero <adrianperezherrero1997@outlook.es>
2024-12-31 01:25:06 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1262,7 +1262,7 @@ class GPI_HDP():
         if one_sample:
             frac = sum_resp / torch.sum(sum_resp)
         else:
-            frac = sum_resp / sum_resp
+            frac = sum_resp / torch.sum(sum_resp)
         for i in sum_resp:
             if i > 0:
                 M_ = M_ + 1
@@ -1270,7 +1270,7 @@ class GPI_HDP():
             if sum_resp[i] > 0:
                 if sum_resp[i] < 2.0:
                     #elb = elb + gp.return_LDS_param_likelihood(first=True)
-                    elb = elb + gp.return_LDS_param_likelihood(first=True) * frac[i] * 1.0
+                    elb = elb + gp.return_LDS_param_likelihood(first=False) * frac[i] * 1.0
                 else:
                     elb = elb + gp.return_LDS_param_likelihood() * frac[i]
         if one_sample:
