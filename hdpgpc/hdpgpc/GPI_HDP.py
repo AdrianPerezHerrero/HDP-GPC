111cee801fb9b201f09b3cd672e147d630214db0
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-02 03:24:10 +0100

    Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1295,7 +1295,7 @@ class GPI_HDP():
             model_type, recursive_warp, warp_updating, inducing_points, estimation_limit, free_deg_MNIV = self.get_default_options()
         ini_Sigma = var_y_y * 0.1
         #ini_Gamma = var_y_y_ * 0.2
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.5]), var_y_y * 3.0])) * 0.1
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.1]), var_y_y * 1.5])) * 0.1
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
