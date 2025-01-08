4f0d7f8d6bea9e98e2048ecff29ef0bc369c358d
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-08 16:12:32 +0100

    v120. Small change in offline scheme. Adding free_deg_MNIV parameter, different for online and offline scheme. Redefine q_lat for each observation to apply snr correctly.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1266,7 +1266,7 @@ class GPI_HDP():
         for i in sum_resp[:-1]:
             if i > 0:
                 M_ = M_ + 1
-        gp_temp = gpmodels if one_sample else gpmodels[:-1]
+        gp_temp = gpmodels if one_sample else gpmodels
         for i, gp in enumerate(gp_temp):
             if sum_resp[i] > 0:
                 if sum_resp[i] < 2.0:
@@ -1280,7 +1280,7 @@ class GPI_HDP():
         if one_sample:
             return elb# / M_
         else:
-            return elb / M_
+            return elb# / M_
 
     def redefine_default(self, x_trains, y_trains, resp):
         """ Method to compute Sigma and Gamma from a batch of examples and assign it to initial values.
