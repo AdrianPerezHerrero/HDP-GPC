a6108f78017f41114eb3267ccce81c11e8b4fb2e
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-09 11:41:46 +0100

    v125. f_new_potential, now using half q_simple and half q_ + q_lat_.<

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -566,7 +566,7 @@ class GPI_model():
                     Sigma = Sigma + self.Gamma[-1]
                 mean = torch.matmul(C, self.f_star[t])
             else:
-                A, Gamma, C, Sigma = self.get_params(t-1)
+                A, Gamma, C, Sigma = self.get_params(t)
                 if proj:
                     Sigma = Sigma + Gamma
                 mean = torch.matmul(C, self.f_star[t])
