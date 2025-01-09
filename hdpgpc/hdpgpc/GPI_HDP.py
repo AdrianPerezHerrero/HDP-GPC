86db6aa71987613c3d4f6d5e358eddd56ce2f093
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-09 16:15:54 +0100

    v128. f_new_potential, only q_simple + q_lat_, not ordered. Params as v123.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1302,7 +1302,7 @@ class GPI_HDP():
         # Good results using 0.01
         ini_Sigma = var_y_y * 0.015
         #ini_Gamma = var_y_y_ * 0.2
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 3.0])) * 0.015
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 3.0])) * 0.015
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
