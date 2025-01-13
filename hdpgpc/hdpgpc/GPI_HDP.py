41c6868cc45daf6cc6702dd135dac9b89dfce263
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-13 12:32:11 +0100

    v140 (Only 104). Verbose implemented to avoid all annoying tqdm.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1301,17 +1301,17 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01
         # Good results using 0.018
-        ini_Sigma = var_y_y * 0.1
+        ini_Sigma = var_y_y * 0.2
         #ini_Gamma = var_y_y_ * 0.2
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.15]), var_y_y * 2.0])) * 0.1
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.15]), var_y_y * 2.0])) * 0.2
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
         #ini_Sigma = 50.0
         #ini_Gamma = 100.0
-        # if ini_Sigma < ini_Gamma / 2.0:
-        #     ini_Sigma = ini_Sigma * 2.0
-        #     ini_Gamma = ini_Gamma * 2.0
+        if ini_Sigma > 200.0:
+             ini_Sigma = ini_Sigma * 0.3
+             ini_Gamma = ini_Gamma * 0.3
         # ini_Sigma = self.cond_to_torch(np.max([ini_Sigma, 10.0]))
         # ini_Gamma = self.cond_to_torch(np.max(([ini_Gamma, 12.0])))
         #ini_Gamma = self.cond_to_torch(np.max([var_y_y_, var_y_y * 1.5]))
