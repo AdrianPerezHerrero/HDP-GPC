4f8b4400abd216a41b7ecf56a8ab569192cfe4e4
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-15 11:15:52 +0100

    v151. Verbose implemented to avoid all annoying tqdm.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1301,7 +1301,7 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01.
         # Good results using 0.018
-        ini_Sigma = var_y_y * 0.010
+        ini_Sigma = var_y_y * 0.015
         ini_Gamma = var_y_y * 0.020
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 0.9]), var_y_y * 1.8])) * 0.012
         #ini_Sigma = var_y_y * 2.0
