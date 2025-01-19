870798fc000c92e198e5afe6d2618ba4da0f2122
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-19 13:52:30 +0100

    v171. SNR new computation. Lower LDS elbo. Lower hyperparams.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1306,8 +1306,8 @@ class GPI_HDP():
         # Good results using 0.01.
         # Good results using 0.018
         ini_Sigma = var_y_y * 0.020
-        ini_Gamma = self.cond_to_torch(np.max([var_y_y * 1.0, var_y_y_])) * 0.023
-        #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 0.9]), var_y_y * 1.8])) * 0.012
+        #ini_Gamma = self.cond_to_torch(np.max([var_y_y * 1.0, var_y_y_])) * 0.021
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 1.8])) * 0.021
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
