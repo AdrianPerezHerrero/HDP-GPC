08d49983b072673b914ec3cfd2be01c3fc69df78
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-01-21 16:25:16 +0100

    v176. Correction in compute_q_first.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1318,9 +1318,9 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01.
         # Good results using 0.018
-        ini_Sigma = var_y_y * 0.020
+        ini_Sigma = var_y_y * 0.012
         #ini_Gamma = self.cond_to_torch(np.max([var_y_y * 1.0, var_y_y_])) * 0.021
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 1.4])) * 0.022
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.014
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
