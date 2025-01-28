f63ee0d831912f60c8ef26300901766af27e9d32
Adrián Pérez Herrero <adrianperezherrero1997@outlook.es>
2025-01-28 11:05:59 +0100

    v196. Sigma and Gamma std * 0.12, 0.15. 20 n_explore steps.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1351,7 +1351,7 @@ class GPI_HDP():
         # Good results using 0.01.
         # Good results using 0.018
         ini_Sigma = var_y_y * 0.012
-        ini_Gamma = var_y_y_ * 0.015
+        ini_Gamma = var_y_y * 0.015
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.020
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
