0c6b6ab7cc54251e95438a5704350f97774b8e6e
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-04 11:01:53 +0100

    v219. 10 explore steps. sigma 25.0, gamma 35.0. Back to f_star. NO Static and Dynamic factors. New computation of MNIW likelihood.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1358,14 +1358,14 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01.
         # Good results using 0.018
-        ini_Sigma = torch.sqrt(var_y_y) * 0.5
-        ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 3.0]))) * 0.5
+        #ini_Sigma = torch.sqrt(var_y_y) * 0.5
+        #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 3.0]))) * 0.5
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.0])) * 0.020
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
-        #ini_Sigma = 50.0
-        #ini_Gamma = 100.0
+        ini_Sigma = 25.0
+        ini_Gamma = 35.0
         # if ini_Sigma > 200.0:
         #      ini_Sigma = ini_Sigma * 0.3
         #      ini_Gamma = ini_Gamma * 0.3
