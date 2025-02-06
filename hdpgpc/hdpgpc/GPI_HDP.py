4790851e7ad01303c54d0d1401f5d2d895a18c33
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-06 16:05:54 +0100

    v233. New way of computing hyperparams reestimation. More dynamic. Using [:-1] models for LDS but not dividing by M.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1411,7 +1411,7 @@ class GPI_HDP():
         # Good results using 0.01.
         # Good results using 0.018
         ini_Sigma = var_y_y * 1.0
-        ini_Gamma = var_y_y_ * 1.0
+        ini_Gamma = var_y_y * 1.5
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
