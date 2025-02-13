18b9fe8e717d5fb56d1710ecefa1f7bc52e6d3dc
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-13 10:43:51 +0100

    v246. Same as ocean data. New reestimation of hyperparams using min. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in observe.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1420,8 +1420,10 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01.
         # Good results using 0.018
-        ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
-        ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.5
+        # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
+        # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
+        ini_Sigma = var_y_y * 2.0
+        ini_Gamma = var_y_y_ * 2.0
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
