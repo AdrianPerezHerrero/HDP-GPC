135c1727d81148820938cde6765bd463f0435ad5
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-13 18:31:58 +0100

    v250. Same as ocean data. New reestimation of hyperparams using max. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in observe.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1423,7 +1423,7 @@ class GPI_HDP():
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         ini_Sigma = var_y_y * 0.015
-        ini_Gamma = var_y_y_ * 0.015
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.5])) * 0.020
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
