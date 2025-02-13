28afe9a3115baf62a53913d9222e51ee13dcfc81
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-13 09:51:45 +0100

    v245. Same as ocean data. New reestimation of hyperparams using min. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in observe.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -661,9 +661,9 @@ class GPI_HDP():
         self : returns an instance of self.
         """
         # Redefine HDP hyperparams for batch inclusion
-        self.gamma = 2.0
-        self.transAlpha = 2.0
-        self.startAlpha = 2.0
+        self.gamma = 10.0
+        self.transAlpha = 10.0
+        self.startAlpha = 10.0
         self.kappa = 0.0
 
         n_samples = np.array(y_trains).shape[0]
@@ -1408,8 +1408,8 @@ class GPI_HDP():
         gp.full_pass_weighted(x_trains, y_trains[:, :, [0]], resp_red[:, 0], snr=self.snr_norm[:, 0])
         ind_ = -1
         #noise = (torch.mean(torch.diag(gp.Sigma[ind_])) + torch.mean(torch.diag(gp.Gamma[ind_]))) / 2.0
-        var_y_y_ = torch.median(torch.diag(gp.Gamma[ind_]))
-        var_y_y = torch.median(torch.diag(gp.Sigma[ind_]))
+        var_y_y_ = torch.min(torch.diag(gp.Gamma[ind_]))
+        var_y_y = torch.min(torch.diag(gp.Sigma[ind_]))
         # samples = y_trains[:n_f][:, :, 0].T
         # samples_ = y_trains[1:n_f+1][:, :, 0].T
         # var_y_y = torch.mean(torch.diag(torch.linalg.multi_dot([(samples - torch.mean(samples, dim=1)[:,np.newaxis]), (samples - torch.mean(samples, dim=1)[:,np.newaxis]).T])) / n_f)# * 0.15
@@ -1420,8 +1420,8 @@ class GPI_HDP():
         # Good results using 0.02
         # Good results using 0.01.
         # Good results using 0.018
-        ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
-        ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 4.0
+        ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) ** 2.0
+        ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) ** 2.1
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
