6d68207ed695d8ef944b35b713a8504d6788eca7
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-14 16:59:22 +0100

    v255. Same as ocean data. New reestimation of hyperparams using median. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star in latent.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1422,14 +1422,14 @@ class GPI_HDP():
         # Good results using 0.018
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
-        ini_Sigma = var_y_y * 0.2
-        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.5])) * 0.22
+        #ini_Sigma = var_y_y * 0.2
+        #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.5])) * 0.22
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Sigma = var_y_y * 2.0
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
-        #ini_Sigma = self.cond_to_torch(25.0)
-        #ini_Gamma = self.cond_to_torch(35.0)
+        ini_Sigma = self.cond_to_torch(25.0)
+        ini_Gamma = self.cond_to_torch(35.0)
         # if ini_Sigma > 200.0:
         #      ini_Sigma = ini_Sigma * 0.3
         #      ini_Gamma = ini_Gamma * 0.3
