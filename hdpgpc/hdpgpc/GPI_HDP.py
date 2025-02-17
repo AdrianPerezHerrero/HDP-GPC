2e4a10437db4849b0ea407d010851fa7dd54ceb7
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-17 14:42:23 +0100

    v259. Same as ocean data. New reestimation of hyperparams using median. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in latent. static and dynamic factor. Seeds added. free_def_MNIV=10

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -664,8 +664,8 @@ class GPI_HDP():
         self : returns an instance of self.
         """
         # Redefine HDP hyperparams for batch inclusion
-        self.gamma = 0.5
-        self.transAlpha = 5.0
+        self.gamma = 0.2
+        self.transAlpha = 0.2
         self.startAlpha = 2.0
         self.kappa = 0.0
 
