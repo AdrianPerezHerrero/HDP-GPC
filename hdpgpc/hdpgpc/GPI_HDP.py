7fad3c890b833794c5be7ebf7d4285296891f8e1
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-17 14:31:03 +0100

    v259. Same as ocean data. New reestimation of hyperparams using median. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in latent. static and dynamic factor. Seeds added. free_def_MNIV=10

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1371,7 +1371,7 @@ class GPI_HDP():
         if one_sample:
             return elb# / M_
         else:
-            return elb / M_ # / torch.sum(sum_resp)
+            return elb# / M_ # / torch.sum(sum_resp)
 
     def redefine_default(self, x_trains, y_trains, resp):
         """ Method to compute Sigma and Gamma from a batch of examples and assign it to initial values.
@@ -1440,8 +1440,8 @@ class GPI_HDP():
         # ini_Gamma = self.cond_to_torch(np.max(([ini_Gamma, 12.0])))
         #ini_Gamma = self.cond_to_torch(np.max([var_y_y_, var_y_y * 1.5]))
         #ini_Gamma = var_y_y_ * 1.5
-        bound_sigma = (ini_Sigma.item() * 1e-7, ini_Sigma.item() * 1e-1)
-        bound_gamma = (ini_Gamma.item() * 1e-7, ini_Gamma.item() * 1e-1)
+        bound_sigma = (ini_Sigma.item() * 1e-7, 1.0)
+        bound_gamma = (ini_Gamma.item() * 1e-7, 1.0)
         #bound_sigma = (0.1, 20.0)
         #bound_gamma = (0.1, 20.0)
         print("-----------Reestimated ------------", flush=True)
