632bd78dd91fade451b2b8eab4d5c1d2689190d5
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-02-17 16:11:40 +0100

    v259. Same as ocean data. New reestimation of hyperparams using median. Change of hyperparams reestimation. Different HDP hyperparam. Using f_star_sm in latent. static and dynamic factor. Seeds added. free_def_MNIV=10

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1354,10 +1354,10 @@ class GPI_HDP():
             frac = sum_resp / torch.sum(sum_resp)
         else:
             frac = sum_resp / sum_resp
-        for i in sum_resp:
+        for i in sum_resp[:-1]:
             if i > 0:
                 M_ = M_ + 1
-        gp_temp = gpmodels if one_sample else gpmodels
+        gp_temp = gpmodels if one_sample else gpmodels[:-1]
         for i, gp in enumerate(gp_temp):
             if sum_resp[i] > 0:
                 if sum_resp[i] < self.free_deg_MNIV:
