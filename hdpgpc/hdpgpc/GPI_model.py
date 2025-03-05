02c9064ced8f9edacf9df0b0af28765826a15361
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-03-05 17:55:42 +0100

    v278. New linear elbo scheme, now not including an empty cluster. self.gamma = 2.0 self.transAlpha = 2.0 self.startAlpha = 2.0 self.kappa = 0.0
    Median Sigma,Gamma * 0.05,0.07
    
    log_sq_error changed. log_lat_error not changed.

--- hdpgpc/hdpgpc/GPI_model.py
+++ hdpgpc/hdpgpc/GPI_model.py
@@ -288,8 +288,7 @@ class GPI_model():
         #Gamma_inv = torch.linalg.solve(Gamma, self.cond_to_cuda(torch.eye(Gamma.shape[0])))
         err = -1 / 2 * torch.linalg.multi_dot([lat_f.T, Gamma_inv, lat_f]) \
               + torch.linalg.multi_dot([lat_f.T, Gamma_inv, A, lat_f_]) \
-              - 1 / 2 * torch.linalg.multi_dot([lat_f_.T, A.T, Gamma_inv, A, lat_f_])
-              #- 1 / 2 * torch.trace(torch.linalg.multi_dot([A.T, Gamma_inv, A, exp_t_t_])) \
+              - 1 / 2 * torch.trace(torch.linalg.multi_dot([A.T, Gamma_inv, A, exp_t_t_])) \
               #- 1 / 2 * torch.trace(torch.linalg.multi_dot([lat_f_.T, A.T, Gamma_inv, A, lat_f_])) \
               #- 1 / 2 * torch.trace(torch.linalg.multi_dot([Gamma_inv, exp_t_t]))
               # - 1 / 2 * torch.trace(Gamma)
