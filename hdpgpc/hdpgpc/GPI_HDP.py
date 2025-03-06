a82039e5fa014b23806219c00e86504c021d0435
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-03-06 08:21:23 +0100

    v278. New linear elbo scheme, now not including an empty cluster. self.gamma = 2.0 self.transAlpha = 2.0 self.startAlpha = 2.0 self.kappa = 0.0
    Median Sigma,Gamma * 0.05,0.06
    
    log_sq_error changed. log_lat_error not changed.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1452,9 +1452,9 @@ class GPI_HDP():
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         ini_Sigma = var_y_y * 0.050
-        #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.5])) * 0.0021
+        ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.0]), var_y_y * 2.5])) * 0.060
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
-        ini_Gamma = var_y_y * 0.070
+        #ini_Gamma = var_y_y * 0.070
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
         #ini_Gamma = var_y_y_ * 1.0
         #ini_Sigma = self.cond_to_torch(25.0)
