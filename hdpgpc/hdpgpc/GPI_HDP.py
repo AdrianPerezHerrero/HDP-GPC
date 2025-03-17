06f92aed95586c12d61b773c53472ca62fa0ffc3
adrianperezherrero <adrianperezherrero1997@outlook.es>
2025-03-17 16:24:15 +0100

    v314. New linear elbo scheme, just including an empty cluster for linear elbo. Added gamma and rho reestimation. ELbo_linears * q. self.gamma = 1.0 self.transAlpha = 1.0 self.startAlpha = 1.0 self.kappa = 0.0
    median Sigma,Gamma * 0.075, 0.100
    
    log_sq_error changed. log_lat_error not changed.
     LDS first on. Dividing by min M.
    n_explore_steps = 8
    Changed stop criteria cause elbo convergence can miss. Now same resp. Also initial reestimation of rho added. Removed refill during estimate new q.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1512,8 +1512,8 @@ class GPI_HDP():
         # Good results using 0.018.
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
-        ini_Sigma = var_y_y * 0.10
-        ini_Gamma = var_y_y * 0.15
+        ini_Sigma = var_y_y * 0.075
+        ini_Gamma = var_y_y * 0.100
         #ini_Gamma = torch.sqrt(self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.5)
         #ini_Gamma = var_y_y * 0.012
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
