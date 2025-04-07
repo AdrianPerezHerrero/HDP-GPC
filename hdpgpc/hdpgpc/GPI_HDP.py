e0a51cc58f5ae7f17bf5b80753c76127262cf3c3
AdrianPerezHerrero <adrianperezherrero1997@outlook.es>
2025-04-07 11:17:35 +0200

    v368. New linear elbo scheme, just including an empty cluster for linear elbo. Added gamma and rho reestimation. Elbo_linears not divided by n_output and not product by points. transTheta and startTheta natural gradient when post. Reduced sq_err first. Detected another bad estimation. self.gamma = 0.8 self.transAlpha = 0.8 self.startAlpha = 0.8 self.kappa = 0.0
    median Sigma,Gamma * 0.025, 0.055
    Lower sigma noise bound for GP.
    log_sq_error changed. log_lat_error not changed.
    LDS first off. Not divide. Change in respPair computation.
    n_explore_steps = 20
    Changed stop criteria cause elbo convergence can miss. Now same resp. Also initial reestimation of rho added. Removed refill during estimate new q.
    
    Included shared_gp.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1493,7 +1493,7 @@ class GPI_HDP():
         if one_sample:
             return elb #/ M_
         else:
-            return elb / M_ * np.min([np.max([1, M_ - 1]), self.M])
+            return elb #/ M_ * np.min([np.max([1, M_ - 1]), self.M])
 
     def redefine_default(self, x_trains, y_trains, resp):
         """ Method to compute Sigma and Gamma from a batch of examples and assign it to initial values.
@@ -1547,8 +1547,8 @@ class GPI_HDP():
         # Good results using 0.018.
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
-        ini_Sigma = var_y_y * 0.055
-        ini_Gamma = var_y_y * 0.085
+        ini_Sigma = var_y_y * 0.025
+        ini_Gamma = var_y_y * 0.055
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.050
         #ini_Gamma = var_y_y * 0.012
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
