e920802a999a93a474e0cba53076394c985070f6
AdrianPerezHerrero <adrianperezherrero1997@outlook.es>
2025-05-02 10:42:48 +0200

    v405. New linear elbo scheme, just including an empty cluster for linear elbo. Added gamma and rho reestimation. Elbo_linears not divided by n_output and not product by points. transTheta and startTheta natural gradient when post. Reduced sq_err first. Detected another bad estimation and emergency reestimation if needed included. self.gamma = 0.1 self.transAlpha = 0.1 self.startAlpha = 0.1 self.kappa = 0.0
    reestimated Sigma,Gamma std * 0.1, 0.115 (isoelectric part).
    Lower sigma noise bound for GP.
    log_sq_error changed. log_lat_error not changed.
    LDS first off. Not divide. Change in respPair computation.
    n_explore_steps = 20
    Changed stop criteria cause elbo convergence can miss. Now same resp. Also initial reestimation of rho added. Removed refill during estimate new q. Remove resp reestimation after gpmodels computations.
    
    Included shared_gp. Update initial LDS params deactivated.

--- hdpgpc/hdpgpc/GPI_HDP.py
+++ hdpgpc/hdpgpc/GPI_HDP.py
@@ -1639,7 +1639,7 @@ class GPI_HDP():
         # ini_Sigma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         # ini_Gamma = self.cond_to_torch(np.max([var_y_y, var_y_y_])) * 2.0
         ini_Sigma = var_y_y * 0.1
-        ini_Gamma = var_y_y * 0.13
+        ini_Gamma = var_y_y * 0.115
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.0])) * 0.050
         #ini_Gamma = var_y_y * 0.012
         #ini_Gamma = self.cond_to_torch(np.min([np.max([var_y_y_,var_y_y * 1.2]), var_y_y * 2.5])) * 2.0
